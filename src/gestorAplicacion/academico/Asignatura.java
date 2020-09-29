1bd2eb5501266121d1ed7c3d073720c3b7c8a18e
cquinchiar <cquinchiar@unal.edu.co>
2020-09-29 14:35:34 -0500

    Creaciones basica de las clases profesor y asignatura
    En este commit cree los atributos basicos de las clases con sus getters y setters

--- src/gestorAplicacion/academico/Asignatura.java
+++ src/gestorAplicacion/academico/Asignatura.java
@@ -1,11 +1,17 @@
 package gestorAplicacion.academico;
 
 import java.util.ArrayList;
+import gestorAplicacion.perfiles.*;
 
 public class Asignatura {
 	
-	
+	private String nombre;
+	private String descripcion;
+	public byte numero_notas;
+	public float porcentaje_avance;
 	private ArrayList<Grado> grados = new ArrayList<Grado>();
+	private ArrayList<Profesor> profesores = new ArrayList<Profesor>();
+	private ArrayList<Nota> calificaciones = new ArrayList<Nota>();
 
 	public ArrayList<Grado> getGrados() {
 		return grados;
@@ -13,11 +19,6 @@ public class Asignatura {
 	public void setGrados(ArrayList<Grado> grados) {
 		this.grados = grados;
 	}
-	
-	
-	
-	
-	
 	
 	//Metodos
 	
@@ -27,6 +28,30 @@ public class Asignatura {
 		de grados el grado en el que esta la asigantura
 		dandosela desde Grado
 		*/
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public ArrayList<Profesor> getProfesores() {
+		return profesores;
+	}
+	public void setProfesores(ArrayList<Profesor> profesores) {
+		this.profesores = profesores;
+	}
+	public ArrayList<Nota> getCalificaciones() {
+		return calificaciones;
+	}
+	public void setCalificaciones(ArrayList<Nota> calificaciones) {
+		this.calificaciones = calificaciones;
 	}
 	
 }
--- src/gestorAplicacion/perfiles/Profesor.java
+++ src/gestorAplicacion/perfiles/Profesor.java
@@ -1,5 +1,43 @@
 package gestorAplicacion.perfiles;
+import java.util.ArrayList;
+import gestorAplicacion.academico.*;
 
-public class Profesor {
+
+public class Profesor extends Persona{
+	private String titulo_profesional;
+	private String email;
+	private int telefono;
+	private ArrayList<Grado> grado_encargado = new ArrayList<Grado>();
+	private ArrayList<Asignatura> asignaturas = new ArrayList<Asignatura>();
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getTitulo_profesional() {
+		return titulo_profesional;
+	}
+	public void setTitulo_profesional(String titulo_profesional) {
+		this.titulo_profesional = titulo_profesional;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public ArrayList<Grado> getGrado_encargado() {
+		return grado_encargado;
+	}
+	public void setGrado_encargado(ArrayList<Grado> grado_encargado) {
+		this.grado_encargado = grado_encargado;
+	}
+	public ArrayList<Asignatura> getAsignaturas() {
+		return asignaturas;
+	}
+	public void setAsignaturas(ArrayList<Asignatura> asignaturas) {
+		this.asignaturas = asignaturas;
+	}
 
 }
